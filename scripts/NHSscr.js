b9bb481c6776b50c37dbbff6100fc54745cacffe
bunnygollen <69409129+bunnygollen@users.noreply.github.com>
2021-02-23 14:29:30 -0600

    Database update
    Reset old member data, added fall semester checkbox, java file for reset & calculating old member required hours (probation)

--- scripts/NHSscr.js
+++ scripts/NHSscr.js
@@ -1,4 +1,3 @@
-
 function myFunction() 
 {
     var first = document.getElementById("fname").value.toUpperCase();
@@ -6,29 +5,45 @@ function myFunction()
     var last = document.getElementById("lname").value.toUpperCase();
     var xmlhttp = new XMLHttpRequest();
     var result = null;
-    xmlhttp.open("GET", "nhstest.txt", false);
+    xmlhttp.open("GET", "spring_semester_data.txt", false);
     xmlhttp.send();
     if (xmlhttp.status==200) {
       result = xmlhttp.responseText;
     }
     //search for club member information
-    var pos = result.indexOf(first+" "+middle+" "+last); //found person
+    var checked = document.getElementById("fallData").checked;
+    var start=0;
+    if(checked)
+      start=result.indexOf("Fall Semester Data");
+    var pos = result.indexOf(first+" "+middle+" "+last, start); //found person
     var namleng= first.length+middle.length+last.length+3; // length of name
-    var hr = result.indexOf("hr",pos); //hours position (+1)
-    //console.log("hello?");
-    var hours = result.slice(pos+namleng+6,hr); //hours with nhs
-    var wNHS = hr+3; //find number of hours with NHS
-    var withNHS=0;
-    var grade = result.slice(pos+namleng,pos+namleng+2);
-    if(pos<8009)
-        withNHS = result.slice(wNHS , result.indexOf("/10",hr));
+    if(checked) 
+    {
+      var hr = result.indexOf("hr",pos); //hours position (+1)
+      //console.log("hello?");
+      var hours = result.slice(pos+namleng+6,hr); //hours with nhs
+      var wNHS = hr+3; //find number of hours with NHS
+      var withNHS=0;
+      //var grade = result.slice(pos+namleng,pos+namleng+2);
+      withNHS = result.slice(wNHS , result.indexOf("/5",hr));
+      var findSACs = result.indexOf("sac",pos);
+      //console.log("hello?");
+      var SACs = result.slice(findSACs-1,findSACs);
+      var end = result.indexOf("|",pos); 
+      var reason = result.slice(findSACs+4,end);
+    }
     else
-        withNHS = result.slice(wNHS , result.indexOf("/5",hr));
-    var findSACs = result.indexOf("sac",pos);
-    //console.log("hello?");
-    var SACs = result.slice(findSACs-1,findSACs);
-    var end = result.indexOf("|",pos); 
-    var reason = result.slice(findSACs+4,end);
+    {
+      var hr = result.indexOf("/",pos); //hours position (+1)
+      var req = result.slice(hr+1,result.indexOf(" ",hr));
+      var hours = result.slice(pos+namleng+6,hr); //hours with nhs
+      var withNHS = result.slice(result.indexOf("/",hr+1)-1,result.indexOf("/",hr+1));
+      var NHSreq = result.slice(result.indexOf("/",hr+1)+1,result.indexOf(" ",result.indexOf("/",hr+1)));
+      var findSACs = result.indexOf("sac",pos);
+      var SACs = result.slice(findSACs-1,findSACs);
+      var end = result.indexOf("|",pos); 
+      var reason = result.slice(findSACs+4,end);
+    }
     //find number of places volunteered at
     /*if(hours.localeCompare("0")!=0)
     {
@@ -53,35 +68,38 @@ function myFunction()
         document.getElementById("displayName").innerHTML="Hello "+dispName+"!";
         var dues = result.slice(pos+namleng+4,pos+namleng+5);
         //paid dues? (Y/N), only consider if not a senior
-        if(dues.localeCompare("Y")==0)
+        /*if(dues.localeCompare("Y")==0)
         {
             document.getElementById("displayDues").innerHTML="Thank you for paying your dues! :D";
             document.getElementById("displayDues").style.fontWeight="normal";
         }
         else
-        {
+        {*/
             if(dues.localeCompare("Y")!=0)
             {
                 document.getElementById("displayDues").innerHTML="**It seems you have not paid dues. The due date for paying dues has passed. Please message an officer if you think this is a mistake, and/or contact Mr. Humes if you still want to be considered a member.**";
                 document.getElementById("displayDues").style.fontWeight="bold";
             }
-        }
+        //}
         document.getElementById("displayName").style.fontWeight="bold";
         document.getElementById("displayName").style.fontSize="200%";
         document.getElementById("displayName").style.paddingBottom = "1vw";
         document.getElementById("displayName").style.borderBottom = "4px solid #26a89d";
-        document.getElementById("displayHours").innerHTML="Regular Hours: "+hours;
-        if(pos<8009)
+        document.getElementById("catCeleb").style.opacity=0;
+        if(checked)
         {
-            document.getElementById("displaywithNHS").innerHTML="Hours with NHS: "+withNHS+"/10";
+          document.getElementById("displayHours").innerHTML="<b>Regular Hours:</b> "+hours+ " out of 10";
+          document.getElementById("displaywithNHS").innerHTML="<b>Hours with NHS:</b> "+withNHS+" out of 5";
         }
         else
         {
-            document.getElementById("displaywithNHS").innerHTML="Hours with NHS: "+withNHS+"/5";
+          document.getElementById("displayHours").innerHTML="<b>Regular Hours:</b> "+hours+" out of "+req;
+          document.getElementById("displaywithNHS").innerHTML="<b>Hours with NHS:</b> "+withNHS+" out of "+NHSreq;
         }
-        document.getElementById("displaySACs").innerHTML="SACs: "+SACs;
+        document.getElementById("displaySACs").innerHTML="<b>SACs: </b>"+SACs;
         var inthrs = parseInt(hours,10);
         var intSACs = parseInt(SACs,10);
+        var intwNHS = parseInt(withNHS,10);
         //var intNHS = parseInt(withNHS,10);
         //for when we have place requirement
         /*if(inthrs>=15 && places.length>=2)
@@ -93,50 +111,51 @@ function myFunction()
         //var img = document.createElement("img");
         //img.src = "img/eqp/"+this.apparel+"/"+this.facing+"";
         
-        
-        if(intSACs>0)
+        if(checked)
         {
-            document.getElementById("displaySacReason").innerHTML = "Past SACs: "+reason;
-        }
-        if(inthrs>=100 && intSACs<3)
-        {
-            document.getElementById("displayCompletion").innerHTML="Congratulations! You have fulfilled all NHS requirements!";
-            //var src = document.getElementById("catJam");
-            //src.appendChild(img);
-            //document.getElementById("catCeleb").style.opacity=1;
-            var x = document.createElement("IMG");
-            x.setAttribute("src", "https://images-ext-2.discordapp.net/external/S-BwQSix8Irh8aYVyoG0P-vKUu-qMw06RZOSshq87Yo/https/media.discordapp.net/attachments/289170611849396225/740310724584407181/image0-30.gif");
-            x.setAttribute("alt", "catGif");
-            document.getElementById("catJam").appendChild(x); 
-            var button1 = document.createElement("BUTTON");
-            button1.innerHTML = "Celebrate!";
-            button1.style.backgroundColor="#4caf91";
-            button1.style.padding="1vw 2vw";
-            button1.style.marginLeft="3vw";
-            button1.style.marginTop="2.5vw";
-            button1.style.border="none";
-            button1.style.color="white";
-            button1.style.borderRadius="2px";
-            button1.style.fontFamily="'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif";
-            button1.style.position="absolute";
-            document.getElementById('celebrateMusic').volume=.2;
-            button1.onclick=function(){document.getElementById('celebrateMusic').play(); console.log("hello?")};
-            button1.onclick=function(){celebration()};
-            document.getElementById("catJam").appendChild(button1);
+          if(intSACs>0)
+          {
+              document.getElementById("displaySacReason").innerHTML = "<b>Past SACs: </b>"+reason;
+          }
+          if(inthrs>=10 && intSACs<3 && intwNHS==5)
+          {
+              document.getElementById("displayCompletion").innerHTML=
+              "Congrats! You fulfilled NHS requirements in the fall semester."
+              document.getElementById("catCeleb").style.opacity=1;
+          }
+          else
+          {
+              if(intSACs>=3)
+              {
+                  document.getElementById("displayCompletion").innerHTML = "Sorry, but you have been removed from NHS for having 3 or more SACs. Please message an officer if you believe this is a mistake.";
+              }
+              else
+              {
+                document.getElementById("displayCompletion").innerHTML=
+                "It seems you missed something in the fall semester. :("
+                document.getElementById("displayCompletion").style.lineHeight=1.25;
+                
+              }
+          }
         }
         else
         {
-            if(intSACs>=3)
-            {
-                document.getElementById("displayCompletion").innerHTML = "Sorry, but you have been removed from NHS for having 3 or more SACs. Please message an officer if you believe this is a mistake.";
-            }
-            else
-            {
-                document.getElementById("displayCompletion").innerHTML=
-                "Remember, you'll need at least 15 regular volunteer hours (5 with NHS) to fulfill NHS requirements. You'll also need to attend the monthly required meetings.";
-                document.getElementById("displayCompletion").style.lineHeight=1.25;
-                //document.getElementById("catCeleb").style.opacity=0;
-            }
+          if(intSACs>0)
+          {
+              document.getElementById("displaySacReason").innerHTML = "<b>Past SACs: </b>"+reason;
+          }
+          if(inthrs>=req && intSACs<3 && intwNHS>=NHSreq)
+          {
+              document.getElementById("displayCompletion").innerHTML=
+              "<br> Congrats! You fulfilled NHS requirements!"
+          }
+          else
+          {
+              if(intSACs>=3)
+              {
+                  document.getElementById("displayCompletion").innerHTML = "Sorry, but you have been removed from NHS for having 3 or more SACs. Please message an officer if you believe this is a mistake.";
+              }
+          }
         }
     }
     else
@@ -148,145 +167,7 @@ function myFunction()
         else
         alert("Sorry, couldn't find you in the database, check spelling?");
     }
-    //console.log("?????");
     //alert("The form was submitted by "+first+" "+middle+" "+last+" "+result.slice(pos+namleng,pos+namleng+2));
 
 }
-function celebration(){
-var emitterSize = 20,
-dotQuantity = 40,
-dotSizeMin = 6,
-dotSizeMax = 8,
-speed = 2.4,
-gravity = 0.7,
-explosionQuantity = 5,
-emitter = document.querySelector('#emitter'),
-explosions = [],
-currentExplosion = 0,
-container, i, move;
 
-function createExplosion(container) {
-  var tl = new TimelineLite({paused: true}),
-  dots = [],
-  angle, duration, length, dot, i, size, r, g, b;
-  for (i = 0; i < dotQuantity; i++) {
-    dot = document.createElement('div');
-    dots.push(dot);
-    dot.className = 'dot';
-    r = getRandom(30, 255);
-    g = getRandom(30, 230);
-    b = getRandom(30, 230);
-    TweenLite.set(dot, {
-      backgroundColor: 'rgb('+r+','+g+','+b+')',
-      visibility: 'hidden'
-    });
-    size = getRandom(dotSizeMin, dotSizeMax);
-    container.appendChild(dot);
-    angle = getRandom(0.65, 0.85) * Math.PI * 2; // a vector pointed up
-    // get maximum distance from the center, factoring in size of dot, and then pick a random spot along that vector to plot a point
-    length = Math.random() * (emitterSize / 2 - size / 2);
-    duration = 3 + Math.random();
-    // place the dot at a random spot within the emitter, and set its size
-    TweenLite.set(dot, {
-      x: Math.cos(angle) * length, 
-      y: Math.sin(angle) * length, 
-      width: size, 
-      height: size, 
-      xPercent: -50, 
-      yPercent: -50,
-      visibility: 'hidden',
-      force3D: true
-    });
-    tl.to(dot, duration / 2, {
-      opacity: 0,
-      ease: RoughEase.ease.config({
-        points: 20,
-        strength: 1.75,
-        clamp: true
-      })
-    }, 0).to(dot, duration, {
-      visibility: 'visible',
-      rotationX: '-='+getRandom(720, 1440),
-      rotationZ: '+='+getRandom(720, 1440),
-      physics2D: {
-        angle: angle * 180 / Math.PI, // translate radians to degrees
-        velocity: (100 + Math.random() * 250) * speed, // initial velocity
-        gravity: 700 * gravity,
-        friction: getRandom(0.1, 0.15)
-      }
-     }, 0).to(dot, 1.25 + Math.random(), {
-      opacity: 0
-    }, duration / 2);
-  }
-  // hide the dots at the end for improved performance (better than opacity: 0 because the browser can ignore the elements)
-  // console.log('setting', dots);
-  // tl.set(dots, {visibility: 'hidden'});
-  return tl;
-}
-
-function explode(element) {
-  var bounds = element.getBoundingClientRect(),
-  explosion;
-  if (++currentExplosion === explosions.length) {
-    currentExplosion = 0;
-  }
-  explosion = explosions[currentExplosion];
-  TweenLite.set(explosion.container, {
-    x: bounds.left + bounds.width / 2,
-    y: bounds.top + bounds.height / 2
-  });
-  explosion.animation.restart();
-}
-
-function getRandom(min, max) {
-  var rand = min + Math.random() * (max - min);
-  return rand;
-}
-
-function play() {
-  move.play(0);
-  var intervalCount = 0,
-  interval = setInterval(function() {
-    if (intervalCount < 5) {
-      explode(emitter);
-      intervalCount++;
-    } else {
-      clearInterval(interval);
-    }
-  }, 150);
-}
-
-function setup() {
-  for (i = 0; i < explosionQuantity; i++) {
-    container = document.createElement('div');
-    container.className = 'dot-container';
-    document.body.appendChild(container);
-    explosions.push({
-      container: container,
-      animation: createExplosion(container)
-    });
-  }
-  console.log("ran set up");
-  move = new TimelineLite({
-    paused: true
-  }).fromTo(emitter, 0.4, {
-    left: '40%'
-  }, {
-    left: '60%',
-    ease: Linear.easeNone
-  }).fromTo(emitter, 0.4, {
-    left: '60%'
-  }, {
-    left: '40%',
-    ease: Linear.easeNone
-  });
-  
-  document.querySelector('body').onclick = function () {
-    play();
-  };
-  
-  play();
-}
-
-setup();
-}
