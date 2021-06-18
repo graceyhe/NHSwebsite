d8e239643371671a9c8b9080cf296183f4df310f
bunnygollen <69409129+bunnygollen@users.noreply.github.com>
2021-06-17 20:13:16 -0500

    Database edited

--- nhsdata.java
+++ nhsdata.java
@@ -1,3 +1,4 @@
+// unnecessary code by grace he hi there
 import java.util.*;
 import java.io.*;
 public class nhsdata
@@ -5,6 +6,7 @@ public class nhsdata
 	public static void main(String args[]) throws IOException
 	{
 		BufferedReader br = new BufferedReader(new FileReader("spring_semester_data.txt"));
+		//code to reset incoming senior member data
 		//PrintWriter out = new PrintWriter(new File("spring_semester_data_blank"));
 		String loop="";
 		while(!loop.equals("Old Members"))
@@ -17,23 +19,14 @@ public class nhsdata
 		while(!loop.equals(""))
 		{
 			String current = loop;
-			int hrs = (int)Double.parseDouble(current.substring(current.indexOf("12g")+6,current.indexOf("hr")));
-			int required = 10;
-			if(hrs<10)
+			int required = 25;
+			if(Integer.parseInt(current.substring(current.indexOf("sac")-1,current.indexOf("sac")))<3)
 			{
-				required+=(10-hrs)*2;
+				current = current.substring(0,current.indexOf("11g"))+"12g"+current.substring(current.indexOf("11g")+3, current.indexOf("11g")+6)+"0/25 0/5 0sac |";
+				System.out.println(current);
 			}
-			current = current.substring(0,current.indexOf("12g")+6)+"0/"+required+current.substring(current.indexOf("hr")+2,current.indexOf("|")+1);
-			current = current.substring(0,current.indexOf("sac")-1)+"0"+current.substring(current.indexOf("sac"),current.indexOf("sac")+4)+current.substring(current.indexOf("|"));
-			int NHShrs = Integer.parseInt(current.substring(current.indexOf("/5")-1,current.indexOf("/5")));
-			int requiredNHS = 5;
-			if(NHShrs<5)
-			{
-				requiredNHS+=(5-NHShrs)*2;
-			}
-			current = current.substring(0,current.indexOf("/5")-1)+"0/"+requiredNHS+current.substring(current.indexOf("/5")+2);
-			//out.println(current);
-			System.out.println(current);
+			else
+				current = "";
 			loop = br.readLine();
 		}
 		//out.close();
--- scripts/NHSscr.js
+++ scripts/NHSscr.js
@@ -3,7 +3,6 @@ function myFunction()
     var first = document.getElementById("fname").value.toUpperCase();
     var middle = document.getElementById("mname").value.toUpperCase();
     var last = document.getElementById("lname").value.toUpperCase();
-    //console.log(first.slice(first.length-1,first.length));
     if(first.slice(first.length-1,first.length).localeCompare(" ")==0)
     {
       first = first.slice(0,first.length-1);
@@ -24,52 +23,39 @@ function myFunction()
       result = xmlhttp.responseText;
     }
     //search for club member information
+    //if the falldata checkbox is checked
     var checked = document.getElementById("fallData").checked;
     var start=0;
     if(checked)
       start=result.indexOf("Fall Semester Data");
     var pos = result.indexOf(first+" "+middle+" "+last, start); //found person
     var namleng= first.length+middle.length+last.length+3; // length of name
-    if(checked) 
-    {
-      var hr = result.indexOf("hr",pos); //hours position (+1)
-      //console.log("hello?");
-      var hours = result.slice(pos+namleng+6,hr); //hours with nhs
-      var wNHS = hr+3; //find number of hours with NHS
-      var withNHS=0;
-      //var grade = result.slice(pos+namleng,pos+namleng+2);
-      withNHS = result.slice(wNHS , result.indexOf("/5",hr));
-      var findSACs = result.indexOf("sac",pos);
-      //console.log("hello?");
-      var SACs = result.slice(findSACs-1,findSACs);
-      var end = result.indexOf("|",pos); 
-      var reason = result.slice(findSACs+4,end);
-    }
-    else
-    {
-      var hr = result.indexOf("/",pos); //hours position (+1)
-      var req = result.slice(hr+1,result.indexOf(" ",hr));
-      var hours = result.slice(pos+namleng+6,hr); //hours with nhs
-      var withNHS = result.slice(result.indexOf("/",hr+1)-1,result.indexOf("/",hr+1));
-      var NHSreq = result.slice(result.indexOf("/",hr+1)+1,result.indexOf(" ",result.indexOf("/",hr+1)));
-      var findSACs = result.indexOf("sac",pos);
-      var SACs = result.slice(findSACs-1,findSACs);
-      var end = result.indexOf("|",pos); 
-      var reason = result.slice(findSACs+4,end);
-    }
+    var hr = result.indexOf("/",pos); //hours position (+1)
+    var req = result.slice(hr+1,result.indexOf(" ",hr));
+    var hours = result.slice(pos+namleng+6,hr); //hours with nhs
+    var withNHS = result.slice(result.indexOf("/",hr+1)-1,result.indexOf("/",hr+1));
+    var NHSreq = result.slice(result.indexOf("/",hr+1)+1,result.indexOf(" ",result.indexOf("/",hr+1)));
+    var findSACs = result.indexOf("sac",pos);
+    var SACs = result.slice(findSACs-1,findSACs);
+    var end = result.indexOf("|",pos); 
+    var placeEnd = result.indexOf("|", end+1);
+    var reason = result.slice(findSACs+4,end);
+
     //find number of places volunteered at
-    /*if(hours.localeCompare("0")!=0)
+    if(hours.localeCompare("0")!=0)
     {
         
-        var places = result.slice(hr+3,end).toString().split(",");
+        var places = result.slice(end+1,placeEnd).toString().split(",");
+  
+        console.log(places);
         document.getElementById("displayPlaces").innerHTML="You've volunteered at "+places;
     }
     else
     {
         document.getElementById("displayPlaces").innerHTML="Check out the Volunteer page for potential volunteering opportunities!";
-    }*/
-    //alert("Number of hours: "+hours);
-    //console.log(result.indexOf("New Members"));
+    }
+
+    //If they put in a name correctly (aka not just first name lol)
     if(pos!=-1 && namleng>3 && first.length>0 && last.length>0)
     {
         if(first.includes(" "))
@@ -81,49 +67,23 @@ function myFunction()
         document.getElementById("displayName").innerHTML="Hello "+dispName+"!";
         var dues = result.slice(pos+namleng+4,pos+namleng+5);
         //paid dues? (Y/N), only consider if not a senior
-        /*if(dues.localeCompare("Y")==0)
+        if(dues.localeCompare("Y")!=0)
         {
-            document.getElementById("displayDues").innerHTML="Thank you for paying your dues! :D";
-            document.getElementById("displayDues").style.fontWeight="normal";
+          document.getElementById("displayDues").innerHTML="**It seems you have not played dues. Please message an officer if you think this is a mistake.";
+          document.getElementById("displayDues").style.fontWeight="bold";
         }
-        else
-        {*/
-            if(dues.localeCompare("Y")!=0)
-            {
-                document.getElementById("displayDues").innerHTML="**It seems you have not paid dues. The due date for paying dues has passed. Please message an officer if you think this is a mistake, and/or contact Mr. Humes if you still want to be considered a member.**";
-                document.getElementById("displayDues").style.fontWeight="bold";
-            }
-        //}
         document.getElementById("displayName").style.fontWeight="bold";
         document.getElementById("displayName").style.fontSize="200%";
         document.getElementById("displayName").style.paddingBottom = "1vw";
         document.getElementById("displayName").style.borderBottom = "4px solid #26a89d";
         document.getElementById("catCeleb").style.opacity=0;
         document.getElementById("displayCompletion").innerHTML="";
-        if(checked)
-        {
-          document.getElementById("displayHours").innerHTML="<b>Regular Hours:</b> "+hours+ " out of 10";
-          document.getElementById("displaywithNHS").innerHTML="<b>Hours with NHS:</b> "+withNHS+" out of 5";
-        }
-        else
-        {
-          document.getElementById("displayHours").innerHTML="<b>Regular Hours:</b> "+hours+" out of "+req;
-          document.getElementById("displaywithNHS").innerHTML="<b>Hours with NHS:</b> "+withNHS+" out of "+NHSreq;
-        }
+        document.getElementById("displayHours").innerHTML="<b>Regular Hours:</b> "+hours+" out of "+req;
+        document.getElementById("displaywithNHS").innerHTML="<b>Hours with NHS:</b> "+withNHS+" out of "+NHSreq;
         document.getElementById("displaySACs").innerHTML="<b>SACs: </b>"+SACs;
         var inthrs = parseInt(hours,10);
         var intSACs = parseInt(SACs,10);
         var intwNHS = parseInt(withNHS,10);
-        //var intNHS = parseInt(withNHS,10);
-        //for when we have place requirement
-        /*if(inthrs>=15 && places.length>=2)
-        {
-            document.getElementById("displayCompletion").innerHTML="Congratulations! You have fulfilled all NHS requirements!";
-            document.getElementById("catCeleb").style.opacity=1;
-            
-        }*/
-        //var img = document.createElement("img");
-        //img.src = "img/eqp/"+this.apparel+"/"+this.facing+"";
         
         if(checked)
         {
@@ -131,7 +91,7 @@ function myFunction()
           {
               document.getElementById("displaySacReason").innerHTML = "<b>Past SACs: </b>"+reason;
           }
-          if(inthrs>=10 && intSACs<3 && intwNHS==5)
+          if(inthrs>=req && intSACs<3 && intwNHS==5 && result.slice(end,placeEnd).includes(","))
           {
               document.getElementById("displayCompletion").innerHTML=
               "<br> Congrats! You fulfilled NHS requirements in the fall semester."
@@ -140,6 +100,7 @@ function myFunction()
           }
           else
           {
+            //Change the 3 to your new SAC max
               if(intSACs>=3)
               {
                   document.getElementById("displayCompletion").innerHTML = "Sorry, but you have been removed from NHS for having 3 or more SACs. Please message an officer if you believe this is a mistake.";
@@ -159,7 +120,8 @@ function myFunction()
           {
               document.getElementById("displaySacReason").innerHTML = "<b>Past SACs: </b>"+reason;
           }
-          if(inthrs>=req && intSACs<3 && intwNHS>=NHSreq)
+          //Change the 3 to your new SAC max
+          if(inthrs>=req && intSACs<3 && intwNHS>=NHSreq && result.slice(end,placeEnd).includes(","))
           {
               document.getElementById("displayCompletion").innerHTML=
               "<br> Congrats! You fulfilled NHS requirements!"
@@ -179,6 +141,7 @@ function myFunction()
           }
         }
     }
+    //if they didn't put in a name or something
     else
     {
         if(namleng==3)
@@ -196,7 +159,5 @@ function myFunction()
         else
         alert("Sorry, couldn't find you in the database, check spelling?");
     }
-    //alert("The form was submitted by "+first+" "+middle+" "+last+" "+result.slice(pos+namleng,pos+namleng+2));
-
 }
 
